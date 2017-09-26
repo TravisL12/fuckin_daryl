aca4b2bcb2221b509d6527844b91cfdda79e0b27
Travis Lawrence <tlawrence@tumblr.com>
2017-09-26 13:14:18 -0700

    Daryl working

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,45 @@
 'use strict';
 
-const daryl = document.getElementsByTagName('audio')[0];
 const dave = document.getElementById('dave-fucking-grohl');
+const mouth = dave.querySelector('.mouth');
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let source;
 
-dave.addEventListener('mouseover', (e) => {
-    if (daryl) {
-        daryl.play();
-    }
-})
+// Promise for loading sound
+function loadSound (url) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        let buffer;
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+
+        request.onload = function() {
+          audioCtx.decodeAudioData(request.response).then((buffer) => {
+            resolve(buffer);
+          });
+        }
+        request.send();
+    })
+}
+
+function playSound(buffer) {
+  source = audioCtx.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioCtx.destination);
+  source.start(0);
+}
+
+function playDaryl(e) {
+    mouth.classList.add('daryl');
+    loadSound('public/audio/fuckin_daryl.mp3').then((buffer) => {
+        playSound(buffer);
+    });
+}
+
+dave.addEventListener('mouseover', playDaryl);
+dave.addEventListener('touchstart', playDaryl);
 
 dave.addEventListener('mouseleave', (e) => {
-    if (daryl) {
-        daryl.pause();
-        daryl.currentTime = 0;
-    }
+    source.stop();
+    mouth.classList.remove('daryl');
 })
